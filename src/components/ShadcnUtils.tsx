3a5d1c28c44b8e4eb7edc0ab6a94f40a85edf860
JOYDEEP DAS <joydeep.accounts@tuta.io>
2024-02-09 01:22:05 +0530

    Modification > Global.css & Shadcn utils
    Shadcn utils > added open the video panel on click function
    
    Global.css > removed unused codes and line spaces

--- src/components/ShadcnUtils.tsx
+++ src/components/ShadcnUtils.tsx
@@ -1,31 +1,38 @@
-import {HoverCard, HoverCardContent, HoverCardTrigger} from "@/components/ui/hover-card.tsx";
-import {ReactNode} from "react";
+"use client";
 
-import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-} from "@/components/ui/carousel.tsx";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card.tsx";
+import { ReactNode, useState } from "react";
 
-import {Card, CardContent} from "@/components/ui/card.tsx";
+import { Card, CardContent } from "@/components/ui/card.tsx";
+
+import { XCircle } from "lucide-react";
 
 const AboutCard = (
-    {title, content, icon}:
-        {
-            title: string;
-            content: ReactNode;
-            icon?: ReactNode;
-        }) => {
-    return (
-        <div className="hover:underline decoration-black m-1.5 cursor-pointer w-fit">
-            <HoverCard openDelay={500} closeDelay={500} >
-                <HoverCardTrigger className="flex flex-row items-center  gap-3">{title} {icon}</HoverCardTrigger>
-                <HoverCardContent className="w-fit">{content}</HoverCardContent>
-            </HoverCard>
-        </div>
-    );
+  { title, content, icon }:
+    {
+      title: string;
+      content: ReactNode;
+      icon?: ReactNode;
+    }) => {
+
+  const [openTab, setOpenTab] = useState(false);
+
+  const handleClick = (): void => {
+    setOpenTab(!openTab);
+  };
+
+  return (
+    <div
+      className="hover:underline p-0 decoration-black m-1.5 cursor-pointer w-fit"
+      onClick={handleClick}>
+      <HoverCard openDelay={100} closeDelay={0} open={openTab}>
+        <HoverCardTrigger className="flex flex-row items-center  gap-3">
+          {openTab ? "CLOSE" : title } { openTab ? <XCircle /> : icon }
+        </HoverCardTrigger>
+        <HoverCardContent className="w-fit p-1 border-2 border-black mb-6 shadow-lg">{content}</HoverCardContent>
+      </HoverCard>
+    </div>
+  );
 };
 
-export {AboutCard};
+export { AboutCard };
