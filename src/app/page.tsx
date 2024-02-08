c6dfbee48cd9bc7e43f804f93672aee762cd1537
JOYDEEP DAS <joydeep.accounts@tuta.io>
2024-02-09 01:33:26 +0530

    Development > added styles and more contents on homepage
    Page.tsx > added more styles and texts
    
    removed unused codes and imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,64 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper.tsx";
-import {GradientText} from "@/components/TextHighlighters.tsx";
+import { GradientText } from "@/components/TextHighlighters.tsx";
 import { buttonVariants } from "@/components/ui/button.tsx";
-import {ChevronRight, Info} from 'lucide-react';
-import Link from "next/link";
-import {AboutCard } from "@/components/ShadcnUtils";
+import { ChevronRight, Info } from 'lucide-react';
+import { AboutCard } from "@/components/ShadcnUtils";
+import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export default function Home() {
-    return (
-        <>
-            <MaxWidthWrapper
-                className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
 
-                <div
-                    className="mx-auto m-6 md:m-10 lg:m-12 flex max-w-fit items-center justify-center space-x-2 overflow-hidden border border-gray-200 rounded-full bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50 cursor-pointer">
+  return (
+    <>
+      <MaxWidthWrapper
+        className="mb-12 mt-20 flex flex-col items-center justify-center text-center px-1">
 
-                    <p className="text-lg text-slate-800 font-semibold">
-                        <GradientText cssStyles="text-xl md:text-2xl font-semibold">Quest AI</GradientText> is live NOW!
-                    </p>
+        <div
+          className="mx-auto text m-6 md:m-10 lg:m-12 flex max-w-fit items-center justify-center space-x-2 overflow-hidden border border-gray-200 rounded-full bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50 cursor-pointer">
 
-                </div>
+          <div className="flex gap-2 items-center justify-center">
+            <GradientText cssStyles="text-base md:text-xl font-semibold">Quest AI</GradientText>
+            <span className="text-sm md:text-base text-slate-800 font-semibold">is live NOW!</span>
+          </div>
 
-                <h1 className="max-w-7xl text-4xl font-bold md:text-5xl lg:text-6xl text-center">
-                    Now your <GradientText cssStyles="line-through">PDFs</GradientText> can reply,
-                    <br/>
-                    <span className="line-through text-slate-700">unlike your Crush</span>
-                </h1>
+        </div>
 
-                <p className="text-base md:text-xl mt-10 font-light">
+        <h1 className="max-w-7xl text-[1.5rem] sm:text-3xl md:text-5xl font-bold lg:text-6xl text-center">
+          Now your <GradientText cssStyles="line-through">PDFs</GradientText> can reply
+          <br />
+          <span className="line-through text-slate-700">unlike your Crush</span>
+        </h1>
 
-                    <span className="text-black font-medium">Your Documents do not just exist, they engage.</span>
+        <p className="text-xs md:text-base mt-10 font-light px-0 sm:px-2">
 
-                    <br/>
+          <span className="text-black font-medium">Your Documents do not just exist, they engage.</span>
 
-                    Just upload documents and start asking questions.
+          <br />
 
-                    <br/>
+          Just upload documents and start asking questions.
 
-                    <span className="font-medium">Quest AI</span> loves human, promise.
-                </p>
+          <br />
 
-                <Link href="/dashboard"
-                      target="_blank"
-                      className={buttonVariants({size: "lg", className: "mt-7"})}>
-                    Get Started <ChevronRight className="ml-2 h-5 w-5"/>
-                </Link>
+          <span className="font-medium">Quest AI</span> loves human, Promise
+        </p>
 
-                <AboutCard title={"How it works"}
-                           content={<iframe width="560" height="315"
-                                            src="https://www.youtube-nocookie.com/embed/BZP1rYjoBgI?si=PZNX3L4BjUPKMDbw"
-                                            title="YouTube video player"
-                                            allow="accelerometer; loop; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                            allowFullScreen></iframe>}
-                           icon={<Info />}/>
+        <RegisterLink
+          className={buttonVariants({ size: "lg", className: "mt-7" })}>
+          Get Started <ChevronRight className="ml-2 h-5 w-5" />
+        </RegisterLink>
 
-            </MaxWidthWrapper>
+        <AboutCard title={"How it works"}
+          content={<iframe width="400" height="260"
+            src="https://www.youtube-nocookie.com/embed/BZP1rYjoBgI?si=PZNX3L4BjUPKMDbw"
+            title="YouTube video player"
+            allow="accelerometer; loop; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen></iframe>}
+          icon={<Info />} />
 
-            {/*<div>*/}
-            {/*    Display project overview*/}
-            {/*</div>*/}
-        </>
-    );
+      </MaxWidthWrapper>
+
+      {/*<div>*/}
+      {/*    Display project overview*/}
+      {/*</div>*/}
+    </>
+  );
 };
