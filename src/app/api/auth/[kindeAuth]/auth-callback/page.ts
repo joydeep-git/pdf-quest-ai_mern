d36a1c5cd64070d1277cac56452a90e3f1509d4c
JOYDEEP DAS <joydeep.accounts@tuta.io>
2024-02-17 19:29:36 +0530

    Development > Auth roter and connected with App

---
+++ src/app/api/auth/[kindeAuth]/auth-callback/page.ts
@@ -0,0 +1,13 @@
+import { redirect, useRouter, useSearchParams } from "next/navigation";
+
+const page = () => {
+
+  const router = useRouter();
+  
+  const searchParams = useSearchParams();
+
+  const origin = searchParams.get("origin");
+
+};
+
+export default page;
